68af1789d95c999d93b296370899282903dfaa2c
DorianBD <bouchard.dorian@gmail.com>
2024-10-16 11:01:54 +0200

    feat: create sqlite  database

--- gateway/src/main.cpp
+++ gateway/src/main.cpp
@@ -43,23 +43,6 @@ int main() {
         return 1;
     }
 
-    // // create an instance of your own tcp handler
-    // TcpHandler myHandler;
-    //
-    // // address of the server
-    // AMQP::Address address("amqp://guest:guest@localhost/vhost");
-    //
-    // // create a AMQP connection object
-    // AMQP::TcpConnection connection(&myHandler, address);
-    //
-    // // and create a channel
-    // AMQP::TcpChannel channel(&connection);
-    //
-    // // use the channel object to call the AMQP method you like
-    // channel.declareExchange("my-exchange", AMQP::fanout);
-    // channel.declareQueue("my-queue");
-    // channel.bindQueue("my-exchange", "my-queue", "my-routing-key");
-
     return 0;
 }
 
